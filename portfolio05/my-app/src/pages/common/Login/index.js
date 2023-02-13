004d674985d09d9d2ec88ee48ae21b75649f2b0a
Ryan Sousa <Ryan.sousa4000@gmail.com>
2023-02-13 12:26:53 -0300

    testing

--- portfolio05/my-app/src/pages/common/Login/index.js
+++ portfolio05/my-app/src/pages/common/Login/index.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
